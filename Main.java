815d1d0e89baf177220876c4359b128cb4557d7f
C-Ajith-07 <ajithkumarzsttk@gmail.com>
2025-02-07 17:10:12 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Main{
   public static void main(String[] args){
-    System.out.println("hiiii");
+    System.out.println("hi");
+  }
+
+  public static int add(int a,int b){
+    return a+b;
   }
 }
+
